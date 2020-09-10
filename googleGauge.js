fe9b98d3cb501e1480a0a607f687016ae4ecfcad
Developer <Developer@Phils-Mac.local>
2020-09-09 17:54:02 -0700

    er2

--- googleGauge.js
+++ googleGauge.js
@@ -384,8 +384,7 @@
 					redFrom: props.redstart, redTo: props.redmax,
 					 max: props.gaugemax, yellowFrom: props.yellowstart, yellowTo: props.yellowmax, 
 					 greenFrom: props.greenstart, greenTo: props.greenmax,
-					 width: 400,
-					 height: 400,
+					
 					 animation:{
 					   duration: 1000,
 					   easing: 'out'},
