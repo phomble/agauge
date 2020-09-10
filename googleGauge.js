473be845c55fc90c9fe822493c2bd4b791fc2191
Developer <Developer@Phils-Mac.local>
2020-09-09 17:38:47 -0700

    cleanup

--- googleGauge.js
+++ googleGauge.js
@@ -382,7 +382,8 @@
 
 				var options = {
 					redFrom: props.redstart, redTo: props.redmax,
-					 max: props.gaugemax, yellowFrom: props.yellowstart, yellowTo: props.yellowmax, greenFrom: props.greenstart, greenTo: props.greenmax,
+					 max: props.gaugemax, yellowFrom: props.yellowstart, yellowTo: props.yellowmax, 
+					 greenFrom: props.greenstart, greenTo: props.greenmax,
 				chartArea: {
 					// leave room for y-axis labels
 					width: '94%'
